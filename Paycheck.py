898c588fc88be391540d5b62947bc4b7ebecf0f7
Justin Kudela <justinkudela@gmail.com>
2021-12-09 05:58:39 -0500

    Working on object for paycheck.

--- Paycheck.py
+++ Paycheck.py
@@ -7,24 +7,32 @@ PATH = "C:\\Users\\Justin Kudela\\Desktop\\"
 kat_file = PATH + 'TestCheck2.pdf'
 justin_file = PATH + 'TestCheck.pdf'
 
+class Paycheck:
+    def __init__(self, gross_pay, **kwargs):
+        self.gross_pay = gross_pay
+        self.federal_tax = federal_tax
+
+
+
 def kat_paycheck(file):
+    """TODO: create the tuple first again and then clean the data before putting it in the dict."""
     df = tb.read_pdf(file, area=(0,0 , 612, 792), columns=[150, 210, 288], stream=True, guess=True, pages='1', pandas_options={'header': None})
 
-    gross_pay = df[0].loc[14, 2]
-    federal_tax = df[0].loc[18, 2]
-    social_security = df[0].loc[19, 2]
-    medicare = df[0].loc[21, 2]
-    ohio_tax = df[0].loc[22, 2]
-    cbus_tax = df[0].loc[23, 2]
-    short_term_disability = df[0].loc[26, 2]
-    four_01k = df[0].loc[27, 2]
-    net_pay = df[0].loc[28, 2]
-    schwab_deposit = df[0].loc[29, 2]
-    fifth_third_deposit = df[0].loc[30, 2]
+    pay_dict = {
+        "gross_pay": df[0].loc[14, 2],
+        "federal_tax": df[0].loc[18, 2],
+        "social_security": df[0].loc[19, 2],
+        "medicare": df[0].loc[21, 2],
+        "ohio_tax": df[0].loc[22, 2],
+        "cbus_tax": df[0].loc[23, 2],
+        "short_term_disability": df[0].loc[26, 2],
+        "four_01k": df[0].loc[27, 2],
+        "net_pay": df[0].loc[28, 2],
+        "schwab_deposit": df[0].loc[29, 2],
+        "fifth_third_deposit": df[0].loc[30, 2]
+    }
 
-    paycheck_items = (gross_pay, federal_tax, social_security, medicare, ohio_tax, cbus_tax, short_term_disability, four_01k, net_pay, schwab_deposit, fifth_third_deposit)
-
-    return paycheck_items
+    return pay_dict
 
 def justin_paycheck(file):
     df = tb.read_pdf(file, area=(0,0 , 612, 792), columns=[390, 440, 475, 515, 590], stream=True, guess=True, pages='1', pandas_options={'header': None})
