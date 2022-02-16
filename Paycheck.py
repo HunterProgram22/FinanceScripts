0cac4778faf4b20f186d75e3b91225f8b4cbdaee
Justin Kudela <justinkudela@gmail.com>
2022-02-16 05:54:52 -0500

    Trying to get working with new paychecks - should redesign.

--- Paycheck.py
+++ Paycheck.py
@@ -26,6 +26,8 @@ class Paycheck:
         self.net_pay = paycheck_items['net_pay']
         self.schwab_deposit = paycheck_items['schwab_deposit']
         self.fifth_third_deposit = paycheck_items['fifth_third_deposit']
+        self.hsa_contribution = paycheck_items['hsa_contribution']
+        self.fsa_contribution = paycheck_items['fsa_contribution']
         try:
             self.total_health_insurance = paycheck_items['health_insurance'] + paycheck_items['vision_insurance'] + paycheck_items['dental_insurance']
         except KeyError:
@@ -37,7 +39,7 @@ def kat_paycheck(file):
     """With kat's paycheck the lines vary based on earnings throughout the year - the personal holiday moved all lines down one, so need
     to figure out a way to address this, same issue probably for Justin."""
     df = tb.read_pdf(file, area=(0,0 , 612, 792), columns=[150, 210, 288], stream=True, guess=True, pages='1', pandas_options={'header': None})
-    # print(df)
+    print(df)
     pay_dict = {
         "kat_gross_pay": clean_data(df[0].loc[16, 2]),
         "justin_gross_pay": 0.0,
@@ -56,31 +58,30 @@ def kat_paycheck(file):
     }
     return pay_dict
 
+
 @logger.catch
 def justin_paycheck(file):
+    PAY_ITEMS_DICT = {
+        "HEALTH": total_health_data,
+        "DENTAL": total_dental_data,
+        "DEF COMP": four_57b_data,
+        "OPERS": opers_data,
+        "VISION": total_vision_data,
+        "HSA EE": hsa_contribution,
+        "FLEX DEP": fsa_contribution,
+    }
     df = tb.read_pdf(file, area=(0,0 , 612, 792), columns=[390, 440, 475, 515, 590], stream=True, guess=True, pages='1', pandas_options={'header': None})
-    # print(df)
+    print(df)
     for row in range(16):
-        # print(df[0].loc[row, 0])
-        if df[0].loc[row, 0] == "HEALTH":
-            # print("Health is: " + str(df[0].loc[row, 1]))
-            total_health_data = clean_data(df[0].loc[row, 1])
-        if df[0].loc[row, 0] == "DENTAL":
-            # print("Dental is: " + str(df[0].loc[row, 1]))
-            total_dental_data = clean_data(df[0].loc[row, 1])
-        if df[0].loc[row, 0] == "DEF COMP":
-            # print("Def Comp is: " + str(df[0].loc[row, 1]))
-            four_57b_data = clean_data(df[0].loc[row, 1])
-            # print(four_57b_data)
-        if df[0].loc[row, 0] == "OPERS":
-            print("OPERS is: " + str(df[0].loc[row, 1]))
-            opers_data = clean_data(df[0].loc[row, 1])
-        if df[0].loc[row, 0] == "VISION":
-            # print("Vision is: " + str(df[0].loc[row, 1]))
-            total_vision_data = clean_data(df[0].loc[row, 1])
+        for key in PAY_ITEMS_DICT.keys():
+            if df[0].loc[row, 0] == key:
+                column_1_value = df[0].loc[row, 1]
+                print(f"{key} is: {column_1_value}")
+                PAY_ITEMS_DICT[key] = clean_data(df[0].loc[row, 1])
+
 
     pay_dict = {
-        "justin_gross_pay": clean_data(df[0].loc[31, 3]),
+        "justin_gross_pay": clean_data(df[0].loc[35, 3]),
         "kat_gross_pay": 0.0,
         "federal_tax": clean_data(df[0].loc[6, 1]),
         "social_security": 0.0,
@@ -91,12 +92,14 @@ def justin_paycheck(file):
         "four_01k": 0.0,
         "four_57b": four_57b_data,
         "opers": opers_data,
-        "net_pay": clean_data(df[0].loc[33, 3]),
-        "schwab_deposit": clean_data(df[0].loc[17, 4]),
-        "fifth_third_deposit": clean_data(df[0].loc[18, 4]),
+        "net_pay": clean_data(df[0].loc[36, 3]),
+        "schwab_deposit": clean_data(df[0].loc[21, 4]),
+        "fifth_third_deposit": clean_data(df[0].loc[22, 4]),
         "health_insurance": total_health_data,
         "vision_insurance": total_vision_data,
-        "dental_insurance": total_dental_data
+        "dental_insurance": total_dental_data,
+        "hsa_contribution": hsa_contribution,
+        "fsa_contribution": fsa_contribution,
     }
     return pay_dict
 
@@ -127,7 +130,9 @@ def sum_gross_pay(paycheck_list):
     dental_insurance = 0.0
     vision_insurance = 0.0
     total_insurance = 0.0
-    for index, paycheck in enumerate(paycheck_list):
+    hsa_contribution = 0.0
+    fsa_contribution = 0.0
+    for paycheck in paycheck_list:
         justin_gross_pay += paycheck.justin_gross_pay
         kat_gross_pay += paycheck.kat_gross_pay
         print("TEST: " + str(paycheck.federal_tax))
@@ -144,6 +149,8 @@ def sum_gross_pay(paycheck_list):
         schwab_deposit += paycheck.schwab_deposit
         fifth_third_deposit += paycheck.fifth_third_deposit
         total_insurance += paycheck.total_health_insurance
+        hsa_contribution += paycheck.hsa_contribution
+        fsa_contribution += paycheck.fsa_contribution
     print("Justin Gross Pay: " + str(justin_gross_pay))
     print("Kat Gross Pay: " + str(kat_gross_pay))
     print("Federal Tax: " + str(federal_tax))
@@ -159,13 +166,15 @@ def sum_gross_pay(paycheck_list):
     print("Schwab Deposit: " + str(schwab_deposit))
     print("Fifth Third Deposit: " + str(fifth_third_deposit))
     print("Total Insurance: " + str(total_insurance))
+    print(f"HSA Contribution: {hsa_contribution}")
+    print(f"FSA Contribution: {fsa_contribution}")
 
 
 paycheck_list = []
 # The files list below is a list comprehension to make sure the directory isn't added as an object.
 files = [file for file in os.listdir(DIRECTORY) if os.path.isfile(os.path.join(DIRECTORY, file))]
 for file in files:
-    # print(file[:3])
+    print(file[:3])
     if file[:3] == "Cit":
         pay_dict = justin_paycheck(PATH + file)
         paycheck = Paycheck(pay_dict)
@@ -174,5 +183,28 @@ for file in files:
         paycheck = Paycheck(pay_dict)
     paycheck_list.append(paycheck)
 
-# print(paycheck_list)
+print(paycheck_list)
 sum_gross_pay(paycheck_list)
+
+# if df[0].loc[row, 0] == "HEALTH":
+#     # print("Health is: " + str(df[0].loc[row, 1]))
+#     total_health_data = clean_data(df[0].loc[row, 1])
+# if df[0].loc[row, 0] == "DENTAL":
+#     # print("Dental is: " + str(df[0].loc[row, 1]))
+#     total_dental_data = clean_data(df[0].loc[row, 1])
+# if df[0].loc[row, 0] == "DEF COMP":
+#     # print("Def Comp is: " + str(df[0].loc[row, 1]))
+#     four_57b_data = clean_data(df[0].loc[row, 1])
+#     # print(four_57b_data)
+# if df[0].loc[row, 0] == "OPERS":
+#     print("OPERS is: " + str(df[0].loc[row, 1]))
+#     opers_data = clean_data(df[0].loc[row, 1])
+# if df[0].loc[row, 0] == "VISION":
+#     # print("Vision is: " + str(df[0].loc[row, 1]))
+#     total_vision_data = clean_data(df[0].loc[row, 1])
+# if df[0].loc[row, 0] == "HSA EE":
+#     print("HSA contribution is: " + str(df[0].loc[row, 1]))
+#     hsa_contribution = clean_data(df[0].loc[row, 1])
+# if df[0].loc[row, 0] == "FLEX DEP":
+#     print("FSA contribution is: " + str(df[0].loc[row, 1]))
+#     fsa_contribution = clean_data(df[0].loc[row, 1])
